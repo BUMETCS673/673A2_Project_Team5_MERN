a71d45f265f6bdb4ae74b396ca2d3a4b6635830e
Wenhao Tian <wtian16@bu.edu>
2023-10-12 16:28:57 -0400

    update doc schema

--- code/server/model/document.js
+++ code/server/model/document.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const documentSchema = new Schema({
+    document_id: String,
     body: String,
+    summary: String,
+    last_modified: Date,
     author: {
         type: Schema.Types.ObjectId,
         ref: 'User'
