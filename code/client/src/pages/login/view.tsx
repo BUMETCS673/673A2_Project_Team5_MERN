d9f31cf9354b39956374f15f07c6228b938ffae5
wayne <lyc0404@bu.edu>
2023-10-16 16:44:02 -0400

    loginpage

--- code/client/src/pages/login/view.tsx
+++ code/client/src/pages/login/view.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import backgroundImage from './kelly-sikkema-Oz_J_FXKvIs-unsplash.jpg';
+import './LoginView.css'; // Import the CSS file
 
 // Define the User interface with optional properties: picture, name, and sub.
 interface User {
@@ -17,53 +18,16 @@ interface LoginViewProps {
 
 export const LoginView: React.FC<LoginViewProps> = ({ user, handleSignOut }) => {
   return (
-    <div
-      className="App"
-      style={{
-        // Set the background image for the entire view
-        backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100vh',
-      }}
-    >
+    <div className="App" style={{ backgroundImage: `url(${backgroundImage})` }}>
       <header className="App-header">
         {/* Create a white box to contain the main content */}
-        <div
-          style={{
-            backgroundColor: 'white',
-            padding: '40px', // Increased padding for a larger white box
-            borderRadius: '10px',
-            boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-            textAlign: 'center',
-            width: '100%', // Increased width for a larger white box
-            height: '100%', // Increased height for a larger white box
-          }}
-        >
+        <div className="white-box">
           {/* Display the welcome message using the "Georgia" font */}
-          <h1 style={{ textAlign: 'center', fontSize: '3em', fontFamily: 'Georgia' }}>
-            Welcome back
-          </h1>
-          <h2 style={{ textAlign: 'center', fontSize: '2.5em', fontFamily: 'Georgia' }}>
-            Note Ant
-          </h2>
+          <h1>Welcome back</h1>
+          <h2>Note Ant</h2>
 
           {/* If the user is not logged in, display the Google Sign-In button */}
-          {Object.keys(user).length === 0 && (
-            <div
-              id="signInDiv"
-              style={{
-                display: 'flex',
-                justifyContent: 'center',
-                width: '100%',
-                marginBottom: '20px',
-              }}
-            ></div>
-          )}
+          {Object.keys(user).length === 0 && <div id="signInDiv" className="signInDiv"></div>}
 
           {/* If the user is logged in, display their details and a sign-out button */}
           {Object.keys(user).length !== 0 && (
