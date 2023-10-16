362e22b408fc8aa8d347ea63cd036a134e29d88a
wayne <lyc0404@bu.edu>
2023-10-16 16:03:57 -0400

    loginpage

--- code/client/src/pages/login/main.tsx
+++ code/client/src/pages/login/main.tsx
@@ -11,17 +11,23 @@ interface User {
   [key: string]: any;
 }
 
-const Main: React.FC = () => {
+const Login: React.FC = () => {
+  // State to hold the user's information
   const [user, setUser] = useState<User>({});
 
+  // Callback function to handle the response after Google Sign-In
   const handleCallbackResponse = (response: any) => {
     console.log('Encoded JWT ID token: ' + response.credential);
+    // Decode the JWT token to get user's information
     const user_object = jwt_decode(response.credential) as User;
     console.log(user_object);
+    // Update the user state with the decoded information
     setUser(user_object);
   };
 
+  // Function to handle user sign out
   const handleSignOut = (event: any) => {
+    // Reset the user state to empty
     setUser({});
     // Re-render the Google Sign-In button after signing out
     google.accounts.id.renderButton(document.getElementById('signInDiv'), {
@@ -30,19 +36,23 @@ const Main: React.FC = () => {
     });
   };
 
+  // UseEffect hook to initialize Google Sign-In button on component mount
   useEffect(() => {
+    // Initialize Google Sign-In with client ID and callback function
     google.accounts.id.initialize({
       client_id: GSI_CLIENT_ID,
       callback: handleCallbackResponse,
     });
 
+    // Render the Google Sign-In button
     google.accounts.id.renderButton(document.getElementById('signInDiv'), {
       theme: 'outline',
       size: 'large',
     });
   }, []);
 
+  // Render the LoginView component and pass user and handleSignOut as props
   return <LoginView user={user} handleSignOut={handleSignOut} />;
 };
 
-export default Main;
+export default Login;
