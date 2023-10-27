3333e7da6b4993fe551f097355d53d8fa3a30b98
Nicholas Narmada <nichnarmada@gmail.com>
2023-10-27 06:47:10 -0400

    added document route to app.js

--- code/server/app.js
+++ code/server/app.js
@@ -3,51 +3,53 @@
 // }
 
 //starts express
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 const app = express();
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 
 //for server status report
 //morgan helps dev by auto generating request details,
 //response time, status code, route in terminal
 
 //for data communication between client port and server port
-const cors = require('cors')
+const cors = require('cors');
 
 const corsOptions = {
   origin: 'http://localhost:5173', // This should be the port Vite is using
-}
-app.use(cors(corsOptions))
-app.use(express.json())
+};
+app.use(cors(corsOptions));
+app.use(express.json());
 
 //mongoDB: mongoose helps to connect to MongoDB
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const dbUrl = 'mongodb://127.0.0.1:27017/teamFive'
-mongoose.connect(dbUrl)
-const db = mongoose.connection
-db.on('error', console.error.bind(console, 'mongo connection error'))
+const dbUrl = 'mongodb://127.0.0.1:27017/teamFive';
+mongoose.connect(dbUrl);
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'mongo connection error'));
 db.once('open', () => {
-  console.log('mongo Connection open!')
-})
+  console.log('mongo Connection open!');
+});
 
 //Data Models
-const User = require('./model/users')
-const Docs = require('./model/document')
+const User = require('./model/users');
+const Docs = require('./model/document');
 
 //Router Fiele
-const userRoute = require('./routes/user')
+const userRoute = require('./routes/user');
+const documentRoute = require('./routes/document');
 
 //Routes
 //app.use("/", loginRoute)
 //app.use('/home', registerRoute)
-app.use('/user', userRoute)
+app.use('/user', userRoute);
+app.use('/document', documentRoute);
 
 app.get('*', (req, res) => {
-  res.send('invalid url')
-})
+  res.send('invalid url');
+});
 
 app.listen(8000, () => {
-  console.log('listening on port 8000')
-})
+  console.log('listening on port 8000');
+});
