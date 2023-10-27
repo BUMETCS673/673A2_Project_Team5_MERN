0513a490f4f26753a9c26607774fc52ed649f564
Nicholas Narmada <nichnarmada@gmail.com>
2023-10-27 06:46:56 -0400

    made openai into an exportable function

--- code/server/ai/ai.js
+++ code/server/ai/ai.js
@@ -4,27 +4,29 @@ import * as dotenv from "dotenv"
 
 dotenv.config()
 
-const llm = new OpenAI({
-  openAIApiKey: process.env.OPENAI_API_KEY,
-  // temperature: 0,
-  maxRetries: 10,
-})
+module.exports.useOpenAi = (input) => {
+  const llm = new OpenAI({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+    // temperature: 0,
+    maxRetries: 10,
+  })
 
-// Prompting
-const prompt = new PromptTemplate({
-  inputVariables: ["notes"],
-  template:
-    "You are a helpful study assistant that summarises {notes} into a summary of bullet points.",
-})
+  // Prompting
+  const prompt = new PromptTemplate({
+    inputVariables: ["notes"],
+    template:
+      "You are a helpful study assistant that summarises {notes} into a summary of bullet points.",
+  })
 
-const formattedPrompt = await prompt.format({
-  // notes is received from the input in frontend
-  // notes: getNotesContent,
-  notes: "a statistics course about linear regression",
-})
+  const formattedPrompt = await prompt.format({
+    // notes is received from the input in frontend
+    // notes: getNotesContent,
+    notes: input,
+  })
 
-console.log(formattedPrompt)
+  // console.log(formattedPrompt)
 
-const llmResult = await llm.call(formattedPrompt)
+  const llmResult = await llm.call(formattedPrompt)
 
-console.log(llmResult)
+  return llmResult
+}
