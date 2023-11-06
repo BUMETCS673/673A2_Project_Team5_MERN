e3751bb3712f284a09b30e5ef54f7c5bfba573ec
Wenhao Tian <wtian16@bu.edu>
2023-11-06 13:41:20 -0500

    Back: genID_create_doc

--- code/server/controller/user.js
+++ code/server/controller/user.js
@@ -1,37 +1,8 @@
 const User = require('../model/users');
-const Docs = require('../model/document');
-//import { v4 as uuidv4 } from 'uuid';
+const Document = require('../model/document');
+const { v4: uuid } = require('uuid')
 const imgSrc = 'https://www.dunkindonuts.com/content/dam/dd/img/menu-redesign/donuts/VanillaFrostedSprinklesDonut_570x570.png'
-const docs = [
-    {
-        imageSrc: imgSrc,
-        title: 'This is a Doc Title 1',
-        description: 'This is a Doc summary 1',
-        linkURL: 'https://www.google.com/',
-        _id: 1
-    },
-    {
-        imageSrc: imgSrc,
-        title: 'This is a Doc Title 2',
-        description: 'This is a Doc summary 2',
-        linkURL: 'https://www.google.com/',
-        _id: 2
-    },
-    {
-        imageSrc: imgSrc,
-        title: 'This is a Doc Title 3',
-        description: 'This is a Doc summary 3',
-        linkURL: 'https://www.google.com/',
-        _id: 3
-    },
-    {
-        imageSrc: imgSrc,
-        title: 'This is a Doc Title 4',
-        description: 'This is a Doc summary 4',
-        linkURL: 'https://www.google.com/',
-        _id: 4
-    }
-]
+
 module.exports.getUser = async (req, res) => {
     try {
         //const userID = req.headers.authorization;
@@ -39,19 +10,18 @@ module.exports.getUser = async (req, res) => {
         //const user = await User.findById(userID); //which will be the sub from google JWT
         //const ßdocs = await Document.find({ author: userID });
 
-        // const sub = req.body.sub;
-        // const user = await User.find({ user_id: sub });
-        // if (user) {
-        //     console.log("user successfully found");
-        //     console.log(user[0]._id)
-        //     console.log(user);
-
-        // }
-        // const docs = await Docs.find({ author: user[0]._id });
-        // if (docs) {
-        //     console.log("docs successfully found");
-        //     console.log(docs)
-        // }
+        const sub = req.body.sub;
+        const user = await User.find({ user_id: sub });
+        if (user) {
+            console.log("user successfully found");
+            console.log(user[0]._id)
+            console.log(user);
+        }
+        const docs = await Document.find({ author: user[0]._id });
+        if (docs) {
+            console.log("docs successfully found");
+            console.log(docs)
+        }
         res.json({ docs });
 
     } catch (err) {
@@ -64,25 +34,19 @@ module.exports.getUser = async (req, res) => {
 module.exports.createDoc = async (req, res) => {
 
     try {
-        // const userID = req.params.userid
-        // const docTitle = req.body.title;
-        // const newDoc = await new Document({
-        //     document_id: uuidv4(),
-        //     title: docTitle,
-        //     author: userID,
-        //     last_modified: new Date()
-        // }).save();
-        const { title } = req.body;
+        const docTitle = req.body.title;
+        const user = await User.find({ user_id: req.body.sub }); // user returned from MongoDB is an array with 1 user
+        const newDoc = await new Document({
+            title: docTitle,
+            document_id: uuid(),
+            content: '',
+            summary: '',
+            last_modified: new Date(),
+            author: user[0]._id,
+            date_modified: new Date()
+        }).save();
 
-        docs.push({
-            imageSrc: imgSrc,
-            title: title,
-            description: 'This is a new document: ' + title,
-            linkURL: 'https://www.google.com/',
-            _id: docs.length + 1
-        })
-
-        res.json({ docs })
+        res.json({ document_id: newDoc.document_id });
 
     } catch (err) {
         console.log(err);
@@ -112,3 +76,39 @@ module.exports.deleteDoc = async (req, res) => {
         return res.json({ error: "Error occur! Unable to delete the doc" })
     }
 }
+
+
+
+
+
+
+// const docs = [
+//     {
+//         imageSrc: imgSrc,
+//         title: 'This is a Doc Title 1',
+//         description: 'This is a Doc summary 1',
+//         linkURL: 'https://www.google.com/',
+//         _id: 1
+//     },
+//     {
+//         imageSrc: imgSrc,
+//         title: 'This is a Doc Title 2',
+//         description: 'This is a Doc summary 2',
+//         linkURL: 'https://www.google.com/',
+//         _id: 2
+//     },
+//     {
+//         imageSrc: imgSrc,
+//         title: 'This is a Doc Title 3',
+//         description: 'This is a Doc summary 3',
+//         linkURL: 'https://www.google.com/',
+//         _id: 3
+//     },
+//     {
+//         imageSrc: imgSrc,
+//         title: 'This is a Doc Title 4',
+//         description: 'This is a Doc summary 4',
+//         linkURL: 'https://www.google.com/',
+//         _id: 4
+//     }
+// ]
