be5613a03edf34a0cf674f207aff67c3cb9abe41
Nicholas Narmada <nichnarmada@gmail.com>
2023-11-07 15:52:49 -0500

    created api path for token verification

--- code/server/routes/login.js
+++ code/server/routes/login.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const requireLogin = require('../middleware/requireLogin.js')
-const loginController = require('../controller/login.js')
+const verifyToken = require('../middleware/verifyToken.js');
+const loginController = require('../controller/login.js');
 
 const router = express.Router();
 
-router.post('/login', loginController); 
+router.post('/login', loginController);
+router.post('/verify-token', verifyToken);
 
 module.exports = router;
