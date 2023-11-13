0f9792445687bc8223dd30cf70050e8546df775f
wayne <lyc0404@bu.edu>
2023-11-13 16:53:47 -0500

    1st

--- code/server/routes/user.js
+++ code/server/routes/user.js
@@ -16,3 +16,16 @@ router.delete('/delete-doc/:docId/:userId', /*verifyToken,*/ ctrl.deleteDoc);
 
 export default router;
 
+
+const express = require('express');
+
+const router = express.Router();
+const ctrl = require('../controller/user');
+const verifyToken = require('../middleware/verifyToken');
+
+//user_id, which is sub, will be includede in the URL
+router.get('/', verifyToken, ctrl.getUser);
+router.post('/create-new-doc', /*verifyToken,*/ ctrl.createDoc);
+router.delete('/delete-doc/:docId', /*verifyToken,*/ ctrl.deleteDoc);
+
+module.exports = router;
