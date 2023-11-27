7f3176f5bd7fff8806d73ede08509c515aed358d
Wenhao Tian <wtian16@bu.edu>
2023-11-27 16:19:37 -0500

    new try

--- code/client/src/hooks/authContext.tsx
+++ code/client/src/hooks/authContext.tsx
@@ -26,52 +26,50 @@ export const AuthProvider: React.FC = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState<User>();
 
-  useEffect(() => {
-    const checkToken = async () => {
-      const token = localStorage.getItem('accessToken');
-      if (token) {
-        try {
-          const response = await axios.post(
-            'http://localhost:8000/current-user',
-            {
-              headers: {
-                Authorization: token,
-              },
-            }
-          );
+  // useEffect(() => {
+  //   const checkToken = async () => {
+  //     const token = localStorage.getItem('accessToken');
+  //     if (token) {
+  //       try {
+  //         const response = await axios.post(
+  //           'http://localhost:8000/current-user',
+  //           {
+  //             headers: {
+  //               Authorization: token,
+  //             },
+  //           }
+  //         );
 
-          console.log('response', response);
-          if (response.data) {
-            const user_object: GoogleUser = jwt_decode(token);
+  //         console.log('response', response);
+  //         if (response.data) {
+  //           const user_object: GoogleUser = jwt_decode(token);
 
-            setIsAuthenticated(true);
-            setUser({
-              user_name: user_object.name,
-              user_pic: user_object.picture,
-              user_id: user_object.sub,
-            });
-          } else {
-            // Token invalid, force log out and navigate to login page
-          }
-        } catch (error) {
-          console.error('Error verifying token:', error);
-        }
-      } else {
-        setIsAuthenticated(false);
-        // navigate('/login')
-      }
-    };
+  //           setIsAuthenticated(true);
+  //           setUser({
+  //             user_name: user_object.name,
+  //             user_pic: user_object.picture,
+  //             user_id: user_object.sub,
+  //           });
+  //         } else {
+  //           // Token invalid, force log out and navigate to login page
+  //         }
+  //       } catch (error) {
+  //         console.error('Error verifying token:', error);
+  //       }
+  //     } else {
+  //       setIsAuthenticated(false);
+  //       // navigate('/login')
+  //     }
+  //   };
 
-    checkToken();
-  }, [isAuthenticated]);
+  //   checkToken();
+  // }, []);
 
-  axios.defaults.headers.common['authorization'] = localStorage.getItem('accessToken');
 
   const login = (userData: User) => {
     setIsAuthenticated(true);
     console.log("userData", userData);
     setUser(userData);
-    console.log('user', user); //printing user here will not work because it is asynchronous
   };
 
   const logout = () => {
--- code/server/routes/document.js
+++ code/server/routes/document.js
@@ -3,11 +3,12 @@
 
 import express from 'express';
 import ctrl from '../controller/document.js';
+import verifyToken from '../middleware/verifyToken.js';
 
 const router = express.Router()
 
-router.get('/:docId/', ctrl.getDocument)
-router.post('/:docId/update-content', ctrl.updateContent)
-router.post('/:docId/update-summary', ctrl.updateSummary)
+router.get('/:docId/', verifyToken, ctrl.getDocument)
+router.post('/:docId/update-content', verifyToken, ctrl.updateContent)
+router.post('/:docId/update-summary', verifyToken, ctrl.updateSummary)
 
 export default router;
