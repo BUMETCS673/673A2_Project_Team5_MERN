57d22274934f6a415d34c04d60a97b71a40b92b0
Wenhao Tian <wtian16@bu.edu>
2023-11-27 14:34:29 -0500

    header now display current user

--- code/client/src/components/Header.tsx
+++ code/client/src/components/Header.tsx
@@ -4,9 +4,13 @@ import { Avatar, Menu } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 import { IconOutlet, IconChevronDown } from '@tabler/icons-react';
 import './Header.css';
-import { user } from '../constants/user';
+// import { user } from '../constants/user';
+import { AuthContext } from '../hooks/authContext';
+import { useContext } from 'react';
+
 
 export default function Header() {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
   //destruct
   const [userMenuOpened, setUserMenuOpened] = useState(false);
@@ -37,7 +41,7 @@ export default function Header() {
               <Group gap={7}>
                 <Avatar src="user.name" alt="" radius="xl" size={60} />
                 <Text fw={500} size="sm" lh={1} mr={3}>
-                  {user.name}
+                  {user?.user_name || 'user'}
                 </Text>
                 <IconChevronDown style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
               </Group>
