066882595d7e3ff3c28d53c84eb220a89188104a
Wenhao Tian <wtian16@bu.edu>
2023-12-06 16:25:17 -0500

    stopped printing token and user

--- code/server/middleware/verifyToken.js
+++ code/server/middleware/verifyToken.js
@@ -6,7 +6,7 @@ const verifyToken = async (req, res, next) => {
   //authorization is the accessToken, and it comes with quotes marks
   //using split to get rid of the quotes marks
   const accessToken = req.headers.authorization;
-  console.log("token from verifyToken", accessToken);
+  //console.log("token from verifyToken", accessToken);
 
   if (!accessToken) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
@@ -15,7 +15,7 @@ const verifyToken = async (req, res, next) => {
   try {
     const decoded = jwt.verify(accessToken, secretKey);
     req.user = decoded;
-    console.log("From verifyToken, req.user", req.user);
+    //console.log("From verifyToken, req.user", req.user);
     next();
   } catch (err) {
     res.status(403).json({ message: 'Invalid token.' });
